b65355e2f2a17405a1a1e947d6832a7a999397d6
Alex Gashkov <gashkov321@gmail.com>
2018-10-02 23:32:34 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.team-carousel').slick({
+     prevArrow: '<i class="fas fa-angle-left left-arrow"></i>',
+     nextArrow: '<i class="fas fa-angle-right right-arrow"></i>',
+  });
+});
